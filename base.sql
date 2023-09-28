e197f8b3170e4fa52896454027e30e5205c376d4
mmm4m4m <108784364+mmm4m4m@users.noreply.github.com>
2023-09-28 09:45:31 +0300

    Add new file "base.sql"

---
+++ base.sql
@@ -0,0 +1,94 @@
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(100)
+  hashed_password VARCHAR(100)
+);
+
+CREATE TABLE clients(
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE employees(
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER,
+  position VARCHAR(50) NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE admins(
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE products(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL 
+);
+
+CREATE TABLE orders(
+  id SERIAL PRIMARY KEY,
+  employee_id INTEGER,
+  client_id INTEGER,
+  total_price FLOAT(10,2),
+  status VARCHAR(255),
+  FOREIGN KEY (employee_id) REFERENCES employees(id)
+  ON DELETE CASCADE ON UPDATE NO ACTION,
+  FOREIGN KEY (client_id) REFERENCES clients(id)
+  ON DELETE CASCADE ON UPDATE NO ACTION
+); 
+
+CREATE TABLE dishes(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  price FLOAT(10,2)
+);
+
+CREATE TABLE product_dish(
+  id SERIAL PRIMARY KEY,
+  product_id INTEGER,
+  dish_id INTEGER,
+  FOREIGN KEY (product_id) REFERENCES products(id)
+  ON DELETE CASCADE ON UPDATE NO ACTION,
+  FOREIGN KEY (dish_id) REFERENCES dishes(id)
+  ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE records(
+  id SERIAL PRIMARY KEY,
+  admin_id INTEGER,
+  title VARCHAR(255),
+  description TEXT,
+  FOREIGN KEY (admin_id) REFERENCES admins(id)
+  ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE supplies(
+  id SERIAL PRIMARY KEY, 
+  admin_id INTEGER,
+  date DATE,
+  FOREIGN KEY (admin_id) REFERENCES admins(id)
+  ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE supply_product (
+  id SERIAL PRIMARY KEY,
+  supply_id INTEGER,
+  product_id INTEGER,
+  price FLOAT(10,2),
+  quantity INTEGER,
+  FOREIGN KEY (supply_id) REFERENCES supplies(id)
+  ON DELETE CASCADE ON UPDATE NO ACTION,
+  FOREIGN KEY (admin_id) REFERENCES admins(id)
+  ON DELETE CASCADE ON UPDATE NO ACTION
+);
